43bb97dbaf90db9b718b63ee32fcabd83f16d2f9
pHTwitch <dominik.pluta2020@gmail.com>
2025-01-22 10:38:39 +0100

    splashscreen added

---
+++ lib/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  final VoidCallback onFinish;
+
+  const SplashScreen({Key? key, required this.onFinish}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Simulate a delay for the splash screen
+    Future.delayed(Duration(seconds: 3), onFinish);
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/background-image.png',
+              fit: BoxFit.cover, // Ensure the image covers the entire screen
+            ),
+          ),
+          Center(
+            child: Image.asset('assets/images/Flappy_Logo.png'), // Your logo image
+          ),
+        ],
+      ),
+    );
+  }
+}
