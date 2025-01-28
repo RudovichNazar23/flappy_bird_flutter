ffe44bb0734fae36294bca3d8024e8904da0f72f
RudovichNazar23 <n.rudovich2005@gmail.com>
2025-01-28 09:53:52 +0100

    added grass to the map

---
+++ lib/components/grass.dart
@@ -0,0 +1,25 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutterbird/game_logic.dart';
+import 'dart:async';
+import '../constants.dart';
+
+class Grass extends SpriteComponent with CollisionCallbacks, HasGameRef<FlutterBird> {
+  Grass(Vector2 position, Vector2 size)
+      : super(position: position, size: size);
+
+  @override
+  FutureOr<void> onLoad() async{
+    sprite = await Sprite.load('grass.png');
+
+    add(RectangleHitbox());
+  }
+
+  @override
+  void update(double dt) {
+    position.x -= groundSpeed * dt;
+    if(position.x + size.x <= 0){
+      removeFromParent();
+    }
+  }
+}
