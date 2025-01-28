4075c186378bd1e32d618552e0f171b6d79a2898
DEKochurov <danylo.kochurov@gmail.com>
2025-01-28 10:03:38 +0100

    fixed range between pipes

--- lib/components/pipe_manager.dart
+++ lib/components/pipe_manager.dart
@@ -6,13 +6,13 @@ import 'pipe.dart';
 
 class PipeManager extends Component with HasGameRef<FlutterBird> {
   double pipeSpawnTimer = 0;
+  bool isFirstSpawn = true;
 
   @override
   void update(double dt) {
-    super.update(dt);
     pipeSpawnTimer += dt;
 
-    if (pipeSpawnTimer > PipeInterval) {
+    if (pipeSpawnTimer > gameRef.pipeSpawnDistance / gameRef.groundSpeed) {
       pipeSpawnTimer = 0;
       spawnPipe();
     }
@@ -21,23 +21,23 @@ class PipeManager extends Component with HasGameRef<FlutterBird> {
   void spawnPipe() {
     final double screenHeight = gameRef.size.y;
     final double maxPipeHeight = screenHeight - groundHeight - PipeGap - minPipeHeight;
-    final double bottomPipeHeight =
-        minPipeHeight + Random().nextDouble() * (maxPipeHeight - minPipeHeight);
+    final double bottomPipeHeight = minPipeHeight + Random().nextDouble() * (maxPipeHeight - minPipeHeight);
     final double topPipeHeight = screenHeight - groundHeight - bottomPipeHeight - PipeGap;
 
+    final double spawnX = isFirstSpawn ? gameRef.size.x : gameRef.size.x;
+
     final bottomPipe = Pipe(
-      Vector2(gameRef.size.x, screenHeight - groundHeight - bottomPipeHeight),
+      Vector2(spawnX, screenHeight - groundHeight - bottomPipeHeight),
       Vector2(pipeWidth, bottomPipeHeight),
       isTopPipe: false,
     );
 
     final topPipe = Pipe(
-      Vector2(gameRef.size.x, 0),
+      Vector2(spawnX, 0),
       Vector2(pipeWidth, topPipeHeight),
       isTopPipe: true,
     );
 
-    // Add pipes to the game
     gameRef.add(bottomPipe);
     gameRef.add(topPipe);
   }
--- lib/game_logic.dart
+++ lib/game_logic.dart
@@ -23,8 +23,9 @@ class FlutterBird extends FlameGame with TapDetector, HasCollisionDetection, Key
   late PipeManager pipeManager;
   late ScoreText scoreText;
   final double groundSpeed;
+  final double pipeSpawnDistance;
 
-  FlutterBird({required this.groundSpeed, required this.playerName});
+  FlutterBird({required this.groundSpeed, required this.playerName, required this.pipeSpawnDistance});
 
   int score = 0;
   bool isGameOver = false;
